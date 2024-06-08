bd3502081a9798a01e4f2b9c0891ae5a8d30f90f
Deepanshimann <deepanshimann96@gmail.com>
2024-06-08 23:25:44 +0100

    Made 4 more sections with animations

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    navAnimation();
-});
+
 function navAnimation(){
-    var nav = document.querySelector("nav");
+    var nav = document.querySelector("nav h4");
     nav.addEventListener("mouseenter", function() {
         let tl = gsap.timeline();
         tl.to(".nav-bottom", {
@@ -42,3 +40,65 @@ function navAnimation(){
         });
     });  
 }
+//page 2 Animation
+function page2imageAnimation(){
+    var rightElems=document.querySelectorAll(".right-elem")
+    rightElems.forEach(function(elem){
+        elem.addEventListener("mouseenter",function(){
+            gsap.to(elem.childNodes[3],{
+                opacity:1,
+                scale:1
+            })
+        })
+        elem.addEventListener("mouseleave",function(){
+            gsap.to(elem.childNodes[3],{
+                opacity:0,
+                scale:0
+            })
+        })
+        elem.addEventListener("mousemove",function(dets){
+         gsap.to(elem.childNodes[3],{
+    x:dets.x-elem.getBoundingClientRect().x-90,
+    y:dets.y-elem.getBoundingClientRect().y-130
+         })
+        })
+    })
+}
+
+function page3VideoAnimation(){
+    var page3Center=document.querySelector(".page3-center")
+    var video=document.querySelector("#page3 video")
+    page3Center.addEventListener("click",function(){
+        video.play()
+        gsap.to(video,{
+            transform:"scaleX(1) scaleY(1)",
+            opacity:1,
+            borderRadius:0
+        })
+    })
+    video.addEventListener("click",function(){
+        video.pause()
+        gsap.to(video,{
+            transform:"scaleX(0.7) scaleY(0)",
+            opacity:0,
+            borderRadius:"30px"
+        })
+    })
+}
+
+var sec1=document.querySelectorAll(".sec-right")
+sec1.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity=1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity=0
+        elem.childNodes[3].load()
+    })
+})
+navAnimation()
+page2imageAnimation()
+page3VideoAnimation()
+
+
